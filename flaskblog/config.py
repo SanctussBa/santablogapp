aefa3b29ceb2ab5410421c77ca0ef725b2ca6891
SanctussBa <santa.baltmane@outlook.com>
2020-09-04 13:54:57 +0200

    change config file

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -10,13 +10,12 @@ class Config(object):
     ENV = ''
     if ENV == 'dev':
         DEBUG = True
-        # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-        SECRET_KEY = 'mysecret'
-        SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:berzone@localhost/blogdata'
+        SECRET_KEY = ' ' #Need to add
+        SQLALCHEMY_DATABASE_URI = 'postgresql://server_name:password@localhost/table_name' #need to change
     else:
         DEBUG = False
         SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
         DEBUG = False
-        SECRET_KEY = 'mysecret'
+
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
