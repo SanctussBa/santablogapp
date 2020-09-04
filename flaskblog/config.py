cac41f9cef25626113e8ef33e8d213740d2d8afe
SanctussBa <santa.baltmane@outlook.com>
2020-09-04 13:59:37 +0200

    setting up secret key variable in config file

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -14,8 +14,9 @@ class Config(object):
         SQLALCHEMY_DATABASE_URI = 'postgresql://server_name:password@localhost/table_name' #need to change
     else:
         DEBUG = False
+        SECRET_KEY = os.urandom(32)
+        app.config['SECRET_KEY'] = SECRET_KEY
         SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-        DEBUG = False
 
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
